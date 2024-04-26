ce4dab4f947072984c59060ed6bd7ad97b76078d
mr.one.id <101521147+onepunya@users.noreply.github.com>
2024-04-26 12:07:35 +0700

    Add files via upload

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import api from './api.js';
+import path from 'path';
 
 const routes = Router();
-
-routes.get('/', (_, res) => res.status(200).json({ status: 'Ok', message: 'Otakudesu unofficial API, made by rzkfyn with <3' }));
+routes.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, './index.html'));
+});
 
 routes.use('/v1', api);
 routes.use((_, res) => res.status(404).json({ status: 'Error', message: 'There\'s nothing here ;_;' }));
